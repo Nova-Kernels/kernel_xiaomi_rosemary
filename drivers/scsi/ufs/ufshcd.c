0601a9420d468b31eda463373ca8e9cd15e8ee18
VoidDev-0 <dev.avax@protonmail.com>
2022-04-28 20:10:55 +0200

    drivers/scsi: ufs: ufshcd: Fix -Wdeclaration-after-statement warning
    Also remove unused variable
    
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9195,8 +9195,8 @@ static int ufshcd_resume(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 	int retry = 3;
 
 	/* MTK PATCH: Lock deepidle/SODI @enter UFS resume callback */
-	ufshcd_vops_deepidle_lock(hba, true);
 	enum ufs_dev_pwr_mode old_pwr_mode;
+	ufshcd_vops_deepidle_lock(hba, true);
 
 	hba->pm_op_in_progress = 1;
 	old_link_state = hba->uic_link_state;
